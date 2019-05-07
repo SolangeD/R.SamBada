c349c2b741087de95eea9ec174aab8983c7100f4
SolangeD <solange.duruz@epfl.ch>
2019-05-07 13:49:33 +0200

    Add files via upload

--- R/Postprocessing.R
+++ R/Postprocessing.R
@@ -455,7 +455,7 @@ plotResultInteractive = function(preparedOutput, varEnv, envFile,species=NULL, p
       }else {
         selectSNP=subset[which(subset$xcoord==f$x),'snp']
         selectSNP=selectSNP[1]
-        otherVar=sambadaOutput[sambadaOutput$snp==selectSNP$snp,]
+        otherVar=sambadaOutput[sambadaOutput$snp==selectSNP,]
         otherVar=data.frame('Marker'=otherVar$Marker, 'Var'=otherVar$Env_1, 'p/q-value'=otherVar[[valueName]])
         otherVar
       }
